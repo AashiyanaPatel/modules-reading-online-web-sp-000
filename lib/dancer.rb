19806c1fa47718d88e5324dcb2d63d54b00b3d83
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-03-10 18:13:51 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,8 +4,6 @@ require_relative './dance_module.rb'
 class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
-  extend MetaDancing
-  include Dance
   
   attr_accessor :name
   
